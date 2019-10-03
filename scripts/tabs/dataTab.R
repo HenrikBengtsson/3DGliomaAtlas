c3e3442846a32cff07f8742d8d67426c59652f30
srhilz <stephanie.hilz@gmail.com>
2019-10-02 22:22:07 -0700

    changed main output name to model3D

--- scripts/tabs/dataTab.R
+++ scripts/tabs/dataTab.R
@@ -5,23 +5,28 @@ tabPanel(title = "Data", id = "data",
   sidebarLayout(
     sidebarPanel(
       
+      # User input - select dataset
       selectInput("dataset",
                   label = "Dataset",
                   choices = c("RNA"),
                   selected = "RNA"),
       
+      # User input - select patient ID
       uiOutput("patientUI"),
       
+      # User input - select tumor
       uiOutput("tumorUI"),
       
+      # User input - select gene
       uiOutput("geneUI"),
       
+      # Display data selected (at some point we can make this look a bit nicer and add in sample IDs)
       tags$p("Vector:"), # Placeholder
       
       verbatimTextOutput("temp_print_text") # Placeholder
     ),
     
-    mainPanel(plotOutput("map"))
+    mainPanel(webGLOutput("model3D"))
   )
   
 )
