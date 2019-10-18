623bcc23d4d91900d3e55627b1a57e5365ab7c61
srhilz <stephanie.hilz@gmail.com>
2019-10-18 12:32:00 -0700

    collapsed plotting into single call for samples and single for text; also added brain plotting

--- scripts/display/plot3Dmodel.R
+++ scripts/display/plot3Dmodel.R
@@ -7,51 +7,46 @@ library(rgl)
 library(gtools)#might not need, test
 library(dplyr)#might not need, test
 
-plotTemplate <- function(tumorModel){
-  print('Processing tumor model')
+plotTemplate <- function(tumorModel, brainModel){
   dtemp <- dim(tumorModel)
+  print('Creating brain contour and plotting brain')
+  brain <- contour3d(brainModel, x=1:dtemp[1], y=1:dtemp[2], z=1:dtemp[3], level = 1, alpha = .05, add = FALSE, draw = TRUE, color = '#B08883')
   print('Creating tumor contour and plotting tumor')
-  tumor <- contour3d(tumorModel, x=1:dtemp[1], y=1:dtemp[2], z=1:dtemp[3], level = 1, alpha = .2, add = FALSE, draw = TRUE, color = 'yellow')
+  tumor <- contour3d(tumorModel, x=1:dtemp[1], y=1:dtemp[2], z=1:dtemp[3], level = 1, alpha = .2, add = TRUE, draw = TRUE, color = 'yellow')
 }
 
-plot3dSamples <- function(sampleModels, colors){
-  for (name in names(sampleModels)){
+plot3dSamples <- function(sampleCoordinates, colors){
+  sampleColors <- c()
+  for (name in rownames(sampleCoordinates)){
     if(!is.na(colors[as.character(name)])){
-      sampleColor <- colors[name]
+      singleColor <- colors[name]
     } else {
-      sampleColor <- '#FFFF00'
+      singleColor <- '#FFFF00'
       print(name)
     }
-    x <- sampleModels[[name]]
-    print(paste0('Processing sample',name))
-    dtemp <- dim(x)
-    sample <- contour3d(x, x=1:dtemp[1], y=1:dtemp[2], z=1:dtemp[3], level = 1, alpha = 1, color=sampleColor, add = TRUE, draw = TRUE)
-    text3d(which(x == 1, arr.ind=TRUE)[1,], texts = name, cex=1, adj=-.3)
+    sampleColors <- append(sampleColors, singleColor)
   }
+  points3d(x=sampleCoordinates[,1], y=sampleCoordinates[,2], z=sampleCoordinates[,3], level = 1, cex = 2, color=sampleColor)
+  text3d(x=sampleCoordinates[,1], y=sampleCoordinates[,2], z=sampleCoordinates[,3], texts = rownames(sampleCoordinates), cex=1, adj=-.3)
 }
+
 
 plot3DmodelMain <- function(patientID, sf, colors){
   # Specify patient and load the config file for that patient. Config file contains paths to imaging files + ordering of samples + sample names + colors
   modelsPath <- paste0('data/models/',patientID,'/',sf)
   
-  # Retreive sample model files
-  sampleModelFiles <- mixedsort(list.files(modelsPath, pattern="sample*"))
-  
-  # Create names object
-  names <- gsub('.rds','',gsub('sample','',sampleModelFiles))
-  
   # Read in sample models
-  sampleModels <- lapply(paste0(modelsPath, '/', sampleModelFiles), readRDS)#this should be a list of all subdirectories in the main sample dir, each containing dicoms for a single data point
-  
-  # Name sample models object 
-  names(sampleModels) <- names
+  sampleCoordinates <- readRDS(paste0(modelsPath, '/sample_coordinates.rds'))
   
   # Read in tumor model for patient
   tumorModel <- readRDS(paste0(modelsPath, '/tumor_t2.rds'))
   
+  # Read in brain model for patient
+  brainModel <- readRDS(paste0(modelsPath, '/brain.rds'))
+  
   # Plot background of brain and tumor
-  plotTemplate(tumorModel) 
+  plotTemplate(tumorModel, brainModel) 
   
   # Plot samples
-  plot3dSamples(sampleModels, colors)
+  plot3dSamples(sampleCoordinates, colors)
 }
